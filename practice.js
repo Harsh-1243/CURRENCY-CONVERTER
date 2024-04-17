39c1f7388c8dd08837763228237bb43fa1e44736
Harsh-1243 <127289084+Harsh-1243@users.noreply.github.com>
2024-04-17 11:57:25 +0530

    Update practice.js
    imported countryList in this file

--- practice.js
+++ practice.js
@@ -1,3 +1,5 @@
+import { countryList } from "./list.js";
+
 const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies";
 
 const inputAmount = document.querySelector(".form-control");
